7a3d54acd8aebc7b35e48e1eec7f1edf2fad2c45
Naranbayar <naranbayar@ymail.com>
2015-12-17 10:11:49 +0800

    Chanched twitter library

--- hello/tests.py
+++ hello/tests.py
@@ -1,13 +1,16 @@
 # Run tests with "manage.py test".
 
+from google.appengine.api import users
+from __future__ import unicode_literals
 from django import http
 from django.test import TestCase
-
 from . import views
+import webapp2
 
 class HomeViewTest(TestCase):
     def test_home(self):
         requst = http.HttpRequest()
         response = views.home(request)
+        self.response.headers['Content-Type'] = "text/html; charset=utf-8"
         self.assertEqual(200, response.status_code)
         self.assertIn('Hello World!', response.content)
--- hello/views.py
+++ hello/views.py
@@ -2,35 +2,51 @@
 # dev_appserver.py ContestTwitterMN/
 
 from django import http
-#import tweepy
+# import tweepy
 import feedparser
 from datetime import datetime
+from twitter import *
+import json
 
 def home(request):
-#	auth = tweepy.OAuthHandler("0I87DLCvIvIs8r6fD3iiYaWQv", "FExCvaLZHdHMmtudeFJzgCIf7z94AZG7Zh0gJru3gQDaYlqgcQ")
-#	auth.set_access_token("3219043802-lbrCq80T6NMW5iesaz7rYr7gyWBF1uatbHhBqPM", "BXlVJAHXMdfSPeqMYFh0toPinCh4D4SJcnMpvv1CmIAZe")
+	# auth = tweepy.OAuthHandler("0I87DLCvIvIs8r6fD3iiYaWQv", "FExCvaLZHdHMmtudeFJzgCIf7z94AZG7Zh0gJru3gQDaYlqgcQ")
+	# auth.set_access_token("3219043802-lbrCq80T6NMW5iesaz7rYr7gyWBF1uatbHhBqPM", "BXlVJAHXMdfSPeqMYFh0toPinCh4D4SJcnMpvv1CmIAZe")
 
-
-#	api = tweepy.API(auth)
+	# api = tweepy.API(auth)
 
 	# public_tweets = api.home_timeline()
 
-	# s = ""
+	# tweets = ""
 	# for tweet in public_tweets:
-	# 	s = s + tweet.text
+	# 	tweets = tweets + tweet.text
 
-	# api.update_status(status="Hello World!!!")
+	# api.update_status(status="Hello World!!! It's first tweet from my django app test")
+	t = Twitter(auth=OAuth("3219043802-lbrCq80T6NMW5iesaz7rYr7gyWBF1uatbHhBqPM", 
+							"BXlVJAHXMdfSPeqMYFh0toPinCh4D4SJcnMpvv1CmIAZe", 
+							"0I87DLCvIvIs8r6fD3iiYaWQv", 
+							"FExCvaLZHdHMmtudeFJzgCIf7z94AZG7Zh0gJru3gQDaYlqgcQ"))
 
+	# Get your "home" timeline
+	public_tweets = t.statuses.user_timeline(screen_name="NaranbayarU",count=5)
+	# t.statuses.update(status="First tweet from twitter library")
+	tweets = "<h2>My last 5 tweets</h2><ul>"
+	for tweet in public_tweets:
+		tweets += '<li><a href="https://twitter.com/NaranbayarU/status/' + tweet['id_str'] + '">' + tweet['text'] + '</a></li>'
+	tweets += '</ul>'
+
+	msj = '<center><h1>Tweet about upcoming programming contests from hackerrank calendar. Coming Soon!!!</h1></center>'
 	rss_url = "http://www.hackerrank.com/calendar/feed.rss"
 	feeds = feedparser.parse( rss_url )
 	nowDate = datetime.utcnow()
+	s = "<h2>Current time is : " + nowDate.strftime("%Y-%m-%d %H:%M:%S UTC") + "</h2><br>"
 
-	s = " Time is : " + nowDate.strftime("%Y-%m-%d %H:%M:%S UTC") + "<br>" + "<ul>"
-	for feed in feeds['entries']:
+	t = "<table>" + "<tr><th>Contest Title</th><th>Start Date</th><th>End Date</th></tr>"
+	for feed in reversed(feeds['entries']):
 		if datetime.strptime(feed['starttime'],"%Y-%m-%d %H:%M:%S UTC") > nowDate:
 			# api.update_status(feed['title'] + " will begin soooon")
 			# break
-			s = s + '<li>' + feed['title'] + ' ' + feed['starttime'] + ' ' + feed['endtime'] + '</li>'
-	s = s + "</ul>"
-	
+			t += '<tr><td><a href = "' + feed['url'] + '">' + feed['title'] + '</a></td><td>' + feed['starttime'] + '</td><td>' + feed['endtime'] + '</td></tr>'
+	t += "</table>"
+	s = s + t + tweets + msj
+
 	return http.HttpResponse(s)
