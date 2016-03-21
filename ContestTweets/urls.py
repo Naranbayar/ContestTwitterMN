808d53d02daa4dfe4c8725cac27064f2cbe6b55c
Naranbayar <naranbayar@ymail.com>
2016-03-21 21:25:30 +0800

    add

--- ContestTweets/urls.py
+++ ContestTweets/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'home.views.home'),
+    url(r'^add$', 'home.views.add'),
+    url(r'^map$', 'home.views.map'),
     url(r'^page$', 'home.views.page'),
+    url(r'^add_contest$', 'home.views.add_contest'),
     url(r'^tweetEvening$', 'home.views.tweetEvening'),
     url(r'^tweetMorning$', 'home.views.tweetMorning'),
+    url(r'^tweetAlways$', 'home.views.tweetAlways'),
 )
